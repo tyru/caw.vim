01714e588c1f1242e2a4f14d69e9d7d6a9b1df42
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-06-06 15:03:19 +0800

    Add dts support
    Support neovim

--- macros/generate-ftplugins.sh
+++ macros/generate-ftplugins.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-vim -u NONE -i NONE --not-a-term -e -s -N -X -S macros/generate-ftplugins.vim -c quit
+if command -v nvim >/dev/null; then
+	not_a_term=--headless
+	vi=nvim
+else
+	not_a_term=--not-a-term
+	vi=vim
+fi
+$vi -u NONE -i NONE $not_a_term -e -s -N -X -S macros/generate-ftplugins.vim -c quit
